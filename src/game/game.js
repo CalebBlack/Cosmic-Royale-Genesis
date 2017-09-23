66072e9c75f58f55a146125c856d9806367cbfe2
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-09-23 15:01:33 -0600

    prepare game

--- src/game/game.js
+++ src/game/game.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import setupGame from './setupGame';
 
 class Game extends React.Component {
+  componentDidMount(){
+    setupGame(this.game);
+  }
   render(){
-    return (<p>game placeholder</p>);
+    return (<canvas ref={(gameCanvas)=>{this.game = gameCanvas;}} className='game' id='game'/>);
   }
 }
 export default Game;
---
+++ src/game/setupGame.js
@@ -0,0 +1,4 @@
+function setupGame(gameCanvas){
+
+}
+export default setupGame;
